239574540b9ca52a6f87cdeb15d58d51f7ba8554
Sunho <sunh803@naver.com>
2023-07-19 04:15:08 +0900

    feat: 나의코드clipboard복사

--- LovelyCrane/View/Login/NickNameView.swift
+++ LovelyCrane/View/Login/NickNameView.swift
@@ -4,8 +4,8 @@
 //
 //  Created by 235 on 2023/07/17.
 //
-
-
+//Marmktest용입니다. 이후 사용가능한곳에서
+// MARK: - 파이어베이스관련 연습코드. 필요한곳에서 사용하시면 됩니다.
 import Firebase
 import FirebaseFirestoreSwift
 class NickNameViewModel: ObservableObject {
@@ -24,6 +24,7 @@ struct NickNameView: View {
     @StateObject var viewModel = NickNameViewModel()
     @State var partnerToken: String = ""
     var body: some View {
+
         VStack {
             TextField("Enter your nickname", text: $viewModel.nickname)
                 .padding()
@@ -40,6 +41,14 @@ struct NickNameView: View {
             }, label: {
                 Text("next view")
             })
+            // MARK: - 내UUID클립보드로 옮긴후 복붙.
+            Button {
+                UIPasteboard.general.string = Auth.auth().currentUser?.uid ?? ""
+            } label: {
+                Label(Auth.auth().currentUser?.uid ?? "", systemImage: "doc.on.doc")
+            }
+            .buttonStyle(.bordered)
+            // MARK: - 파트너토큰을 입력하고 클릭을 하게되면 실제 있는 uuid면 서로연결이됨.
             TextField("Enter your partnertoken", text: $partnerToken)
                 .padding()
                 .background(Color(uiColor: .secondarySystemBackground))
