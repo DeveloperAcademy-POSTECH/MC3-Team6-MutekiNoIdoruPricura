42c55cb92f52f1835a8ebe07fcb3d005199d9af8
earlysummer0303 <jw9914@naver.com>
2023-07-25 06:18:01 +0900

    feat: WriteView 모든 로직 완성 및 신규 디자인 반영

---
+++ LovelyCrane/View/WritePage/EnlargedImageView.swift
@@ -0,0 +1,20 @@
+//
+//  EnlargedImageView.swift
+//  LovelyCrane
+//
+//  Created by 황지우2 on 2023/07/25.
+//
+
+import SwiftUI
+
+struct EnlargedImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EnlargedImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnlargedImageView()
+    }
+}
